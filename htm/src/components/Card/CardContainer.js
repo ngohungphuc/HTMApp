282a59736c23f7bf6ea21348b27719a8545fc947
ngohungphuc <ngohungphuc95@gmail.com>
2018-07-03 13:22:04 +0700

    setup component

---
+++ htm/src/components/Card/CardContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import CardDetail from "./CardDetail";
+
+class CardContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default CardContainer
