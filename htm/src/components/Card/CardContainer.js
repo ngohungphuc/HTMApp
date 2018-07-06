ffce149deef686b505294fc00cee71d545d3c090
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-06 16:10:08 +0700

    update code

--- htm/src/components/Card/CardContainer.js
+++ htm/src/components/Card/CardContainer.js
@@ -27,7 +27,8 @@ class CardContainer extends Component {
     render() {
         const {searchStr, rooms} = this.state;
         let filteredRoom = rooms.filter((item) => {
-            return (item.name.toLowerCase().search(searchStr.toLowerCase()) !== -1 || item.description.toLowerCase().search(searchStr.toLowerCase()) !== -1);
+            return (item.name.toLowerCase().search(searchStr.toLowerCase()) !== -1
+                || item.description.toLowerCase().search(searchStr.toLowerCase()) !== -1);
         });
 
         return (
